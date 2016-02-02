f7064c6bac4154e41c3be6aba903704e86a5002c
HO Phu Thai <hophuthai@gmail.com>
2016-02-02 18:25:42 +0100

    Change namespace

--- src/com/thai/utils/popup/ChatPopup.java
+++ src/com/thai/utils/popup/ChatPopup.java
@@ -1,4 +1,4 @@
-package com.example.popup;
+package com.thai.utils.popup;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
--- src/com/thai/utils/popup/PopupActivity.java
+++ src/com/thai/utils/popup/PopupActivity.java
@@ -1,4 +1,4 @@
-package com.example.popup;
+package com.thai.utils.popup;
 
 import android.app.Activity;
 import android.os.Bundle;
